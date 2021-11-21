3c752d0e96289cbc9a35939b16f9025b28b7340d
ivanl <ivanl@cqg.com>
2021-11-21 18:17:37 +0300

    remove dependency

--- test/svetofor.test.js
+++ test/svetofor.test.js
@@ -1,7 +1,6 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const chai = require('chai');
-chai.use(require('chai-bignumber')());
 var BigNumber = require('bignumber.js');
 
 describe("Testing svetofor contract", function() {
