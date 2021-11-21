c6a36c1d78c2c00c066c364b6710903c27943448
ivanl <ivanl@cqg.com>
2021-11-21 12:11:04 +0300

    Add some tests

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+      accounts: []
+    }
+  },
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
--- test/svetofor.test.js
+++ test/svetofor.test.js
@@ -1,20 +1,41 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-let svetoforContract;
-let owner1;
-let owner2;
-let driver1;
-let driver2;
-
-beforeEach(async function() {
-  let svetoforFactory = await ethers.getContractFactory("SwitchForMoney");
-  [ owner1, owner2, driver1, driver2 ] = await ethers.getSigners();
-  svetoforContract = await svetoforFactory.deploy({ from: owner1 });
-});
-
 describe("Testing svetofor contract", function() {
-  it("Owner of the contract should be corrent", async fucntion() {
+  let svetoforContract;
+  let owner1;
+  let owner2;
+  let driver1;
+  let driver2;
+
+  beforeEach(async function() {
+    let svetoforFactory = await ethers.getContractFactory("SwitchForMoney");
+    [ owner1, owner2, driver1, driver2 ] = await ethers.getSigners();
+    svetoforContract = await svetoforFactory.deploy();
+  });
+
+  it('Owner of the contract should be correct', async function() {
+    actualOwner = await svetoforContract.owner();
+    expect(actualOwner).to.be.equal(owner1.address);
   });
   
+  it('Switch should be created off and emit event', async function() {
+    await expect(svetoforContract.CreateSwitch('TestSwitch1'))
+            .to.emit(svetoforContract, 'NewSwitch')
+            .withArgs('TestSwitch1');
+              
+    let isSwitchOn = await svetoforContract.IsOn('TestSwitch1');
+    expect(isSwitchOn).to.be.equal(false);
+  });
+  
+  it('Switch can be created only by owner', async function() {
+    await expect(svetoforContract.connect(owner2).CreateSwitch('TestSwitch1'))
+            .to.be.revertedWith("Only owner is allowed to create new switches.");
+  });
+  
+  it('Should fail creation switch if name already exist', async function() {
+    await svetoforContract.CreateSwitch('TestSwitch1');
+    await expect(svetoforContract.CreateSwitch('TestSwitch1'))
+            .to.be.revertedWith("Switch with specified id already exist. Try other id.");
+  });
 });
