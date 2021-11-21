fe59ea537b2017644baf7bc4b01f18b7beed5d07
ivanl <ivanl@cqg.com>
2021-11-21 18:20:19 +0300

    Revert "Get rid of version in chai-bignumber"
    This reverts commit 2d38c07739e31435e599b57979dcee5a986d0b15.

--- test/svetofor.test.js
+++ test/svetofor.test.js
@@ -1,6 +1,7 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const chai = require('chai');
+chai.use(require('chai-bignumber')());
 var BigNumber = require('bignumber.js');
 
 describe("Testing svetofor contract", function() {
